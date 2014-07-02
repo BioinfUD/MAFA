bec2bb168b2a619dcda76a10e664a4de78e2ce5b
alejo0317 <alejandro.0317@gmail.com>
2014-07-01 23:39:21 -0500

    Check  pep8, removed import on unnecessary script

--- BlastExec.py
+++ BlastExec.py
@@ -1,8 +1,8 @@
-
 import sys
 import os
 import Config
 global threads
+
 
 def usage():
     print """
@@ -10,18 +10,21 @@ def usage():
     """
     exit()
 
+
 def sendblast(query_file, db_file, threads, prefix):
-    command="blastx -query %s -outfmt 5 -db %s -evalue 1e-3 -num_threads %s -out %s" % (query_file, db_file, threads, prefix)
+    command = "blastx -query %s -outfmt 5 -db %s -evalue 1e-3 -num_threads %s -out %s" % (query_file, db_file, threads, prefix)
     print "Excuting alignments with blast"
     os.system(command)
 
+
 def main():
-    if len(sys.argv)!=4:
+    if len(sys.argv) != 4:
         usage()
-    query_file=sys.argv[1]
-    db_file=sys.argv[2]
-    prefix=sys.argv[3]
-    threads=Config.max_threads
+    query_file = sys.argv[1]
+    db_file = sys.argv[2]
+    prefix = sys.argv[3]
+    threads = Config.max_threads
     sendblast(query_file, db_file, threads, prefix)
-    
+
+
 main()
--- GoDistribution.py
+++ GoDistribution.py
@@ -1,73 +1,83 @@
 #This script makes the distribution of GO terms anotated using second level Terms
-global Dict2, obos
-import sys, os
+global Dict2
+global obos
+import sys
+import os
 sys.path.append(os.path.split(sys.argv[0])[0])
-sys.path.append(os.path.split(sys.argv[0])[0]+"/Utilities")
+sys.path.append(os.path.split(sys.argv[0])[0] + "/Utilities")
 sys.path.append("Utilities")
 import GoRelationalFunctions
-import Config
 import StringIO
 import OboParser
 #Function that gives GOID information (OBO imported from DB)
-OBOs=OboParser.parseObo(open("Data/go.obo","r"))
-Dict2={}
+OBOs = OboParser.parseObo(open("Data/go.obo", "r"))
+Dict2 = {}
 
-##@param Gowanted
+
 #Name of file containing the GO terms to serach
 #Initializes the  wanted terms dictionary
 def loadTerms(GOwanted):
-	Data_in=open(GOwanted,"r")
-	global Dict2
-	counter=0
-	for line in Data_in:
-		data_main=line.replace(" ","").replace("\n","")
-		Dict2[data_main]=[]
-	
+    Data_in = open(GOwanted, "r")
+    global Dict2
+    counter = 0
+    for line in Data_in:
+        data_main = line.replace(" ", "").replace("\n", "")
+        Dict2[data_main] = []
+
+
 #Asocciates GO Identifiers with a second level parent and stores results y main Dictionary
 def asocciate(in_file):
-	in_file=open(in_file,"r")
-	for line in in_file:
-		contig_id=line.split(",")[0]
-		if line.split(",")[1]=="NotFound" or line.split(",")[1]=="":
-			pass;
-		gos=line.split(",")[1].replace(" ","").split(";")
-		for goid in gos:
-			go_keys=Dict2.keys()
-			for go_key in go_keys: 
-				if go_key in GoRelationalFunctions.getAncestors(OBOs,goid):
-					Dict2[go_key].append(contig_id)
+    in_file = open(in_file, "r")
+    for line in in_file:
+        contig_id = line.split(",")[0]
+        if line.split(",")[1] == "NotFound" or line.split(",")[1] == "":
+            pass
+        gos = line.split(",")[1].replace(" ", "").split(";")
+        for goid in gos:
+            go_keys = Dict2.keys()
+            for go_key in go_keys:
+                if go_key in GoRelationalFunctions.getAncestors(OBOs, goid):
+                    Dict2[go_key].append(contig_id)
+
 
 #Write ready to plot tables
 def writeTables(Counted_file):
-	out_file=open(Counted_file,'w')
-	for go2 in Dict2.keys():
-		out_file.write('%s\t%s\n' % (go2, str(len(Dict2[go2])) ))
-	return 0
+    out_file = open(Counted_file, 'w')
+    for go2 in Dict2.keys():
+        out_file.write('%s\t%s\n' % (go2, str(len(Dict2[go2]))))
+    return 0
+
+
 #Write go 2 contigs associations
 def writego2contigs(outgo2contigs):
-	out_file=open(outgo2contigs,"w")
-	for go2 in Dict2.keys():
-		out_file.write('%s;%s\n' % (go2, ";".join(Dict2[go2])) )
-	return 0
+    out_file = open(outgo2contigs, "w")
+    for go2 in Dict2.keys():
+        out_file.write('%s;%s\n' % (go2, ";".join(Dict2[go2])))
+    return 0
+
+
 #Another help function
 def usage():
-	print """
-	GoDistribution.py contigs2go.csv GOswanted Counted_GOs.tab go2contigs.csv
-	"""
-	exit()
+    print """
+    GoDistribution.py contigs2go.csv GOswanted Counted_GOs.tab go2contigs.csv
+    """
+    exit()
+
 
 def main():
-	if len(sys.argv)!=5:
-		usage()
-	Counted_file=sys.argv[3]
-	GOwanted=sys.argv[2]
-	in_file=sys.argv[1]
-	go2contigs_file=sys.argv[4]
-	print "Loading terms to asocciate"
-	loadTerms(GOwanted)
-	print "Associating terms to contigs"
-	asocciate(in_file)
-	print "Writing outputs"
-	writeTables(Counted_file)
-	writego2contigs(go2contigs_file)
+    if len(sys.argv) != 5:
+        usage()
+    Counted_file = sys.argv[3]
+    GOwanted = sys.argv[2]
+    in_file = sys.argv[1]
+    go2contigs_file = sys.argv[4]
+    print "Loading terms to asocciate"
+    loadTerms(GOwanted)
+    print "Associating terms to contigs"
+    asocciate(in_file)
+    print "Writing outputs"
+    writeTables(Counted_file)
+    writego2contigs(go2contigs_file)
+
+
 main()
